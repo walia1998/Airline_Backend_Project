746b47ad4bcd2d16f84ecc844c12a2bcadee6ee4
Ashish Walia <waliaashu1998@gmail.com>
2024-09-21 18:04:53 +0530

    Completed city repsitory

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository {
             const city = await City.create({name});
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer")
             throw {error};
         }
     }
@@ -17,7 +18,32 @@ class CityRepository {
                     id: cityId,
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+    async updateCity(cityId, data) {
+        try {
+            const city  = await City.update(data, {
+                where : {
+                    id : cityId,
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city  = await City.findByPK(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
