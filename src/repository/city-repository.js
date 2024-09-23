2cb7bae35a1b45ffe47265430a45852cfca8140d
Ashish Walia <waliaashu1998@gmail.com>
2024-09-23 17:02:50 +0530

    Added Airport Model

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { City } = require("../models/index.js");
 
 class CityRepository {
@@ -62,8 +63,19 @@ class CityRepository {
     }
 
 
-    async getAllCities() {
+    async getAllCities(filter) {
         try {
+
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where : {
+                        name : {
+                            [Op.startsWith] : filter.name
+                        }
+                    }
+                });
+                return cities
+            }
         const cities = await City.findAll();
         return cities; 
         } catch (error) {
